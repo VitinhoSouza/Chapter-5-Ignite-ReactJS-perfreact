6fe19ef92d8311335a612ff78169e234daab824e
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2022-11-15 16:31:03 -0300

    feat: memo, useMemo, useCallback, lazy loading and data formating

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, useState, useCallback } from 'react';
+import { SearchResults } from './components/SearchResults';
+
+type Results = {
+  totalPrice: number;
+  data: any[];
+}
 
 function App() {
+
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState<Results>({
+    totalPrice: 0,
+    data: [],
+  });
+
+  async function handleSearch(event: FormEvent){
+    event.preventDefault();
+
+    if(!search.trim()){
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+    const formatter = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    })
+
+    const products = data.map((product:any) => {
+      return {
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        priceFormatted: formatter.format(product.price),
+      }
+    })
+
+    const totalPrice = data.reduce((total:any, product:any) => {
+        return total + product.price;
+    }, 0);
+
+    setResults({totalPrice,data: products});
+  }
+
+  const addToWishList = useCallback(async (id:number) => {
+    console.log(id);
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input type="text" value={search} onChange={e => setSearch(e.target.value)}/>
+        <button type='submit'>Buscar</button>
+      </form>
+
+      <SearchResults results={results.data} totalPrice={results.totalPrice} onAddToWishList={addToWishList}/>
     </div>
   );
 }
---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,40 @@
+import React, {memo, useState, lazy, Suspense } from "react";
+import { AddProductToWishListProps } from "./AddProductToWishList";
+
+const AddProductToWishList = lazy(()=>{
+    return import("./AddProductToWishList")
+        // .then(({ AddProductToWishList }) => ({ default: AddProductToWishList }))
+        .then((module) => ({default: module.AddProductToWishList}))
+})
+
+interface ProductItemProps{
+    product: {
+        id: number;
+        price: number;
+        title: string;
+        priceFormatted: string;
+    },
+    onAddToWishList: (id:number) => void;
+}
+
+
+export function ProductItemComponent ({product, onAddToWishList}:ProductItemProps){
+
+    const [isAddingToWishList, setIsAddingToWishList] = useState(false);
+    return (
+        <div>
+            {product.title} - <strong>{product.priceFormatted}</strong>
+            <button onClick={()=>setIsAddingToWishList(true)}>Adicionar aos favoritos</button>
+
+            {isAddingToWishList && (
+                <Suspense fallback={<>...</>}>
+                    <AddProductToWishList onAddToWishList={()=>onAddToWishList(product.id)} onRequestClose={()=>setIsAddingToWishList(false)}/>
+                </Suspense>
+            )}
+        </div>
+    )
+}
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps)=>{
+    return Object.is(prevProps.product, nextProps.product);
+});
